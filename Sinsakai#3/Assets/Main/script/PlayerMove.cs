ba7ade3fa2250dd8f53082ec5c12cbde03ef0811
Enulu <enuarle8@icloud.com>
2024-06-15 00:57:21 +0900

    [Add]Lighting,GameManager,Player

--- Sinsakai#3/Assets/Main/script/PlayerMove.cs
+++ Sinsakai#3/Assets/Main/script/PlayerMove.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -6,23 +6,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    /// <summary>
+    /// プレイヤーの移動速度
+    /// </summary>
     [SerializeField] float _movePower = 10;
     
     private Vector3 _dir;
-    // Start is called before the first frame update
+    
     void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        transform.position += _dir * Time.deltaTime * _movePower;
     }
 
     public void OnMove(InputValue value)
     {
         var Axis = value.Get<Vector2>();
         _dir = new Vector3(Axis.x, Axis.y, 0);
-    }
-
-    private void Update()
-    {
-        transform.position += _dir * Time.deltaTime * _movePower;
     }
 }
